9cea3c7378caa5909e6af2fa42b9b6b3a1d9a254
Thomas <technisandk@gmail.com>
2019-12-24 13:30:34 +0100

    Add support for multiple firmware versions and build types

--- README.md
+++ README.md
@@ -10,18 +10,21 @@ The `mods/` folder is for Python code that will be frozen into more efficient by
 This is where you want to place scripts that you'd like freeze to save memory.
 
 ## Docker Container usage
-Copy your custom MicroPython modules to `./mods/` and then run:
+First copy your custom MicroPython modules to `./mods/`.  
+Then run the following commands:
 
 ```
-$ ./build_mpy.sh clean
+$ ./build_mpy.sh 1.11 clean
 
 For the ESP32-WROOM32 with 520kB SRAM:
-	$ ./build_mpy.sh mk-def
+	$ ./build_mpy.sh 1.11 mk-def
 For the ESP32-WROVER-B with 520kB SRAM + 4MB SPI-RAM:
-	$ ./build_mpy.sh mk-spiram
+	$ ./build_mpy.sh 1.11 mk-spiram
 ```
 
 The new firmware binary will be copied to  
 
-- ESP32-WROOM32: `./mpy-firmware-def.bin`
-- ESP32-WROVER-B: `./mpy-firmware-spiram.bin`
+- ESP32-WROOM32: `./mpy-firmware-1.11-def.bin`
+- ESP32-WROVER-B: `./mpy-firmware-1.11-spiram.bin`
+
+If you'd like to compile a different firmware version just replace `1.11` with that version number in the commands above.
--- build_mpy.sh
+++ build_mpy.sh
@@ -45,56 +45,99 @@ VAR_MYDIR="$(dirname "$VAR_MYDIR")"
 
 # ----------------------------------------------------------
 
-VAR_R_BASH=false
-VAR_R_CLEAN=false
-VAR_R_MKALL_DEF=false
-VAR_R_MKALL_SRAM=false
+OPT_R_BASH=false
+OPT_R_CLEAN=false
+OPT_R_MKALL=false
+
+OPT_FWTYPE_DEF=false
+OPT_FWTYPE_SRAM=false
 
 function printUsageAndExit() {
-	echo "Usage: $(basename "$0") bash|clean|mk-def|mk-spiram" >/dev/stderr
+	echo "Usage: $VAR_MYNAME VERSION bash|[clean|mk def|spiram]" >/dev/stderr
+	echo "Examples: $VAR_MYNAME 1.11 bash" >/dev/stderr
+	echo "          $VAR_MYNAME 1.11 clean def" >/dev/stderr
+	echo "          $VAR_MYNAME 1.11 mk def" >/dev/stderr
+	echo "          $VAR_MYNAME 1.12 clean spiram" >/dev/stderr
+	echo "          $VAR_MYNAME 1.12 mk spiram" >/dev/stderr
 	exit 1
 }
 
-if [ $# -ne 1 ]; then
+if [ $# -lt 2 ]; then
+	if [ $# -gt 0 ]; then
+		echo -e "Missing argument. Aborting.\n" >/dev/stderr
+	fi
+	printUsageAndExit
+fi
+
+OPT_MPY_FW_VERS="$1"
+shift
+
+if [ ! -d "${VAR_MYDIR}/$OPT_MPY_FW_VERS/def/mpbuild" ]; then
+	echo -e "Invalid firmware version '$OPT_MPY_FW_VERS'. Aborting.\n" >/dev/stderr
 	printUsageAndExit
 fi
 
 if [ "$1" = "bash" ]; then
-	VAR_R_BASH=true
-elif [ "$1" = "clean" ]; then
-	VAR_R_CLEAN=true
-elif [ "$1" = "mk-def" ]; then
-	VAR_R_MKALL_DEF=true
-elif [ "$1" = "mk-spiram" ]; then
-	VAR_R_MKALL_SRAM=true
+	if [ $# -ne 1 ]; then
+		echo -e "Too many arguments. Aborting.\n" >/dev/stderr
+		printUsageAndExit
+	fi
+	OPT_R_BASH=true
 else
-	printUsageAndExit
+	if [ $# -lt 2 ]; then
+		echo -e "Missing argument. Aborting.\n" >/dev/stderr
+		printUsageAndExit
+	elif [ $# -gt 2 ]; then
+		echo -e "Too many arguments. Aborting.\n" >/dev/stderr
+		printUsageAndExit
+	fi
+	if [ "$1" = "clean" ]; then
+		OPT_R_CLEAN=true
+	elif [ "$1" = "mk" ]; then
+		OPT_R_MKALL=true
+	else
+		echo -e "Invalid argument '$1'. Aborting.\n" >/dev/stderr
+		printUsageAndExit
+	fi
+	shift
+	if [ "$1" = "def" ]; then
+		OPT_FWTYPE_DEF=true
+	elif [ "$1" = "spiram" ]; then
+		OPT_FWTYPE_SRAM=true
+	else
+		echo -e "Invalid argument '$1'. Aborting.\n" >/dev/stderr
+		printUsageAndExit
+	fi
 fi
 
 # ----------------------------------------------------------
 
-LCFG_CONTAINER_NAME="mpy-esp32-cc"
-
 LCFG_IMAGE_NAME="mpy-esp32-cc-amd64"
-LCFG_IMAGE_VER="1.11"
+
+LCFG_CONTAINER_NAME="mpy-esp32-cc-$(echo -n "$OPT_MPY_FW_VERS" | tr -d .)"
 
 LCFG_MNTPOINT_BASE="${VAR_MYDIR}"
-LCFG_MNTPOINT_MPBUILD="${LCFG_MNTPOINT_BASE}/mpbuild"
-LCFG_MNTPOINT_MPSCR="${LCFG_MNTPOINT_BASE}/mpscripts"
-LCFG_MNTPOINT_MODS="${LCFG_MNTPOINT_BASE}/mods"
+LCFG_MNTPOINT_MPBUILD_TEMPL="${LCFG_MNTPOINT_BASE}/${OPT_MPY_FW_VERS}/#TYPE#/mpbuild"
+LCFG_MNTPOINT_MPSCR_TEMPL="${LCFG_MNTPOINT_BASE}/${OPT_MPY_FW_VERS}/#TYPE#/mpscripts"
+
+LCFG_PATH_MODS="${VAR_MYDIR}/mods"
 
 LCFG_MPY_SDKCONFIG_DEF="sdkconfig"
 LCFG_MPY_SDKCONFIG_SRAM="sdkconfig.spiram"
 
-LCFG_MPY_FW_OUTP_FN_TEMPL="mpy-firmware-#TYPE#.bin"
+LCFG_MPY_FW_OUTP_FN_TEMPL="${VAR_MYDIR}/mpy-firmware-${OPT_MPY_FW_VERS}-#TYPE#.bin"
 
 # ----------------------------------------------------------
 
+LVAR_MNTPOINT_MPBUILD=""
+LVAR_MNTPOINT_MPSCR=""
 LVAR_MPY_SDKCONFIG=""
 LVAR_MPY_FW_OUTP_FN=""
 
 # ----------------------------------------------------------
 
+# @param string $1 Username for executing bash (optional)
+# @param string $2 Command to run
 function _runCont() {
 	local TMP_USER="$1"
 	[ -n "$TMP_USER" ] && TMP_USER="-u $TMP_USER"
@@ -105,27 +148,42 @@ function _runCont() {
 			--rm \
 			-it \
 			-d \
-			-v "$LCFG_MNTPOINT_MPBUILD":/esp/micropython/ports/esp32/build \
-			-v "$LCFG_MNTPOINT_MPSCR":/esp/micropython/ports/esp32/scripts \
+			-v "$LVAR_MNTPOINT_MPBUILD":/esp/micropython/ports/esp32/build \
+			-v "$LVAR_MNTPOINT_MPSCR":/esp/micropython/ports/esp32/scripts \
 			$TMP_USER \
-			"$LCFG_IMAGE_NAME":"$LCFG_IMAGE_VER" \
+			"$LCFG_IMAGE_NAME":"$OPT_MPY_FW_VERS" \
 			bash || exit 1
-	if [ "$VAR_R_MKALL_DEF" = "true" -o "$VAR_R_MKALL_SRAM" = "true" ]; then
+	if [ "$OPT_R_MKALL" = "true" ]; then
 		echo "* Copy local modules to Docker Container..."
 		#docker exec "$LCFG_CONTAINER_NAME" rm -r /esp/micropython/ports/esp32/modules
 		#docker exec "$LCFG_CONTAINER_NAME" tar xf /esp/modules-org.tgz -C /esp/micropython/ports/esp32
-		for TMP_FN in "$LCFG_MNTPOINT_MODS"/*; do
+		TMP_MYDIR_SED="$(echo -n "$VAR_MYDIR" | sed -e 's/\//\\\\\\\//g')"
+		for TMP_FN in "$LCFG_PATH_MODS"/*; do
 			TMP_BFN="$(basename "$TMP_FN")"
-			[ "$TMP_BFN" = "do_not_remove" ] && continue
-			[ "$TMP_BFN" = ".DS_Store" ] && continue
-			echo "  -$TMP_FN-"
-			docker cp "$TMP_FN" "$LCFG_CONTAINER_NAME:/esp/micropython/ports/esp32/modules/" || exit 1
+			[ \
+					"$TMP_BFN" = "do_not_remove" -o \
+					"$TMP_BFN" = ".DS_Store" -o \
+					"$TMP_BFN" = ".gitignore" -o \
+					"$TMP_BFN" = ".gitattributes" \
+					] && continue
+			TMP_REL_FN="$(echo -n "$TMP_FN" | sed -e 's/\//\\\//g')"
+			TMP_REL_FN="$(echo -n "$TMP_REL_FN" | sed -e "s/^$TMP_MYDIR_SED/./")"
+			TMP_REL_FN="$(echo -n "$TMP_REL_FN" | sed -e 's/\\//g')"
+			echo "  - $TMP_REL_FN"
+			docker cp \
+					"$TMP_FN" \
+					"$LCFG_CONTAINER_NAME:/esp/micropython/ports/esp32/modules/" || exit 1
 		done
 	fi
 
 	echo "* Run '$LCFG_CONTAINER_NAME' $2..."
-	docker exec "$LCFG_CONTAINER_NAME" \
-			$2
+	if [ "$OPT_R_CLEAN" = "true" ]; then
+		docker exec "$LCFG_CONTAINER_NAME" \
+				$2 >/dev/null 2>&1
+	else
+		docker exec -it "$LCFG_CONTAINER_NAME" \
+				$2
+	fi
 	docker stop "$LCFG_CONTAINER_NAME"
 }
 
@@ -142,38 +200,64 @@ function _runBash() {
 	_runCont "root" "bash"
 }
 
+# Outputs the path to a mountpoint
+#
+# @param string $1 Path template
+function _getMpPath() {
+	local TMP_TYPESTR="def"
+	[ "$OPT_FWTYPE_SRAM" = "true" ] && TMP_TYPESTR="spiram"
+	echo -n "$1" | sed -e "s/#TYPE#/$TMP_TYPESTR/"
+}
+
+# Outputs the firmware filename
+function _getFwOutpFn() {
+	local TMP_TYPESTR="def"
+	[ "$OPT_FWTYPE_SRAM" = "true" ] && TMP_TYPESTR="spiram"
+	echo -n "$LCFG_MPY_FW_OUTP_FN_TEMPL" | sed -e "s/#TYPE#/$TMP_TYPESTR/"
+}
+
+function _removeFwFile() {
+	[ -f "$LVAR_MPY_FW_OUTP_FN" ] && rm -i "$LVAR_MPY_FW_OUTP_FN"
+}
+
 # ----------------------------------------------------------
 
-LVAR_MPY_FW_OUTP_FN="$LCFG_MPY_FW_OUTP_FN_TEMPL"
-if [ "$VAR_R_MKALL_SRAM" = "true" ]; then
+LVAR_MNTPOINT_MPBUILD="$(_getMpPath "$LCFG_MNTPOINT_MPBUILD_TEMPL")"
+LVAR_MNTPOINT_MPSCR="$(_getMpPath "$LCFG_MNTPOINT_MPSCR_TEMPL")"
+LVAR_MPY_FW_OUTP_FN="$(_getFwOutpFn)"
+if [ "$OPT_FWTYPE_SRAM" = "true" ]; then
 	LVAR_MPY_SDKCONFIG="$LCFG_MPY_SDKCONFIG_SRAM"
-	LVAR_MPY_FW_OUTP_FN="$(echo -n "$LVAR_MPY_FW_OUTP_FN" | sed -e "s/#TYPE#/spiram/")"
 else
 	LVAR_MPY_SDKCONFIG="$LCFG_MPY_SDKCONFIG_DEF"
-	LVAR_MPY_FW_OUTP_FN="$(echo -n "$LVAR_MPY_FW_OUTP_FN" | sed -e "s/#TYPE#/def/")"
 fi
 
-if [ "$VAR_R_BASH" = "true" ]; then
+if [ "$OPT_R_BASH" = "true" ]; then
 	_runBash
 	TMP_RES=$?
-elif [ "$VAR_R_CLEAN" = "true" ]; then
+elif [ "$OPT_R_CLEAN" = "true" ]; then
+	_removeFwFile
+	#
 	_runMakeClean
 	TMP_RES=$?
-elif [ "$VAR_R_MKALL_DEF" = "true" -o "$VAR_R_MKALL_SRAM" = "true" ]; then
-	[ -f "$LVAR_MPY_FW_OUTP_FN" ] && rm "$LVAR_MPY_FW_OUTP_FN"
+elif [ "$OPT_R_MKALL" = "true" ]; then
+	_removeFwFile
+	#
 	_runMakeAll
 	TMP_RES=$?
-	if [ $TMP_RES -eq 0 -a -f mpbuild/firmware.bin ]; then
-		echo "* Copy new firmware to '$LVAR_MPY_FW_OUTP_FN'..."
-		cp mpbuild/firmware.bin "$LVAR_MPY_FW_OUTP_FN"
+	#
+	if [ $TMP_RES -eq 0 -a -f "$LVAR_MNTPOINT_MPBUILD/firmware.bin" ]; then
+		echo "* Copying new firmware to '$LVAR_MPY_FW_OUTP_FN'..."
+		cp "$LVAR_MNTPOINT_MPBUILD/firmware.bin" "$LVAR_MPY_FW_OUTP_FN"
+	else
+		echo "! Could not find new firmware binary" >/dev/stderr
 	fi
 else
 	echo "$VAR_MYNAME: unknown command ?!" >/dev/stderr
 	TMP_RES=1
 fi
 
-[ -f "$LCFG_MNTPOINT_MODS/do_not_remove" ] || touch "$LCFG_MNTPOINT_MODS/do_not_remove"
-[ -f "$LCFG_MNTPOINT_MPBUILD/do_not_remove" ] || touch "$LCFG_MNTPOINT_MPBUILD/do_not_remove"
-[ -f "$LCFG_MNTPOINT_MPSCR/do_not_remove" ] || touch "$LCFG_MNTPOINT_MPSCR/do_not_remove"
+[ -f "$LCFG_PATH_MODS/do_not_remove" ] || touch "$LCFG_PATH_MODS/do_not_remove"
+[ -f "$LVAR_MNTPOINT_MPBUILD/do_not_remove" ] || touch "$LVAR_MNTPOINT_MPBUILD/do_not_remove"
+[ -f "$LVAR_MNTPOINT_MPSCR/do_not_remove" ] || touch "$LVAR_MNTPOINT_MPSCR/do_not_remove"
 
 exit $TMP_RES
